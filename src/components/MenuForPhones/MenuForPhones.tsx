97470d36fefd70c0ae02ce2a1bca2e0f4b4dbba4
Guðmundur Snorri Sigfússon <gummehh@gmail.com>
2020-11-21 17:57:01 +0000

    fix(footer): rett numer

--- src/components/MenuForPhones/MenuForPhones.tsx
+++ src/components/MenuForPhones/MenuForPhones.tsx
@@ -1,126 +1,70 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import * as FakeData from '@/db.json';
-import { FoodList } from '@/components/Shared';
+import { Food } from '@/Models';
 import * as MFP from './MenuForPhones.styled';
 
-enum Rettir {
-  Smabraud,
-  donsk,
-  spaensk,
-  adal,
-  tortilla,
-  salot,
-  barna,
-}
-
-const ShowMenu = ({ WhatToShow }): JSX.Element => {
-  const { matsedill } = FakeData;
-  switch (WhatToShow) {
-    case Rettir.Smabraud:
-      return (
-        <>
-          <h1>Smáréttir</h1>
-          <FoodList List={matsedill[`Smáréttir`]} />
-        </>
-      );
-    case Rettir.donsk:
-      return (
-        <>
-          <h1>Dönsk Rúgbrauð</h1>
-          <FoodList List={matsedill[`Dönsk rúgbrauð`]} />
-        </>
-      );
-    case Rettir.spaensk:
-      return (
-        <>
-          <h1>Spænsk Súrdeigsbrauð</h1>
-          <FoodList List={matsedill[`Spænsk súdeigsbrauð`]} />
-        </>
-      );
-    case Rettir.salot:
-      return (
-        <>
-          <h1>Salöt</h1>
-          <FoodList List={matsedill[`Salöt`]} />
-        </>
-      );
-    case Rettir.adal:
-      return (
-        <>
-          <h1>Aðalréttir</h1>
-          <FoodList List={matsedill[`Aðalréttir`]} />
-        </>
-      );
-    case Rettir.tortilla:
-      return (
-        <>
-          <h1>Mjúk Tortilla</h1>
-          <FoodList List={matsedill[`Mjúk Tortilla`]} />
-        </>
-      );
-    case Rettir.barna:
-      return (
-        <>
-          <h1>Barna matseðill</h1>
-          <FoodList List={matsedill[`Barnamatseðill`]} />
-        </>
-      );
-    default:
-      return <div>Hleður</div>;
-  }
+const Rettir = {
+  Smarettir: `Smáréttir`,
+  donsk: `Dönsk rúgbrauð`,
+  spaensk: `Spænsk súdeigsbrauð`,
+  adal: `Aðalréttir`,
+  tortilla: `Mjúk Tortilla`,
+  salot: `Salöt`,
+  barna: `Barnamatseðill`,
 };
+
+const Drykkir = {
+  kaffi: `Kaffi og Te`,
+  oafengt: `Óáfengir drykkir`,
+  bjor: `Bjór`,
+  flosku: `Flöskubjór`,
+  freydi: `Freyðandi Drykkir`,
+  hvitvin: `Hvítvín`,
+  rosavin: `Rósavín`,
+  raudvin: `Rauðvín`,
+};
+
+function getDrinkOrFood() {
+  const { drykkir, matsedill } = FakeData;
+  return useRouter().pathname === `/drykkjarsida` ? drykkir : matsedill;
+}
 
 export function MenuForPhones(): JSX.Element {
   const [show, setShow] = React.useState<boolean>();
-  const [whatToShow, setWhatToShow] = React.useState<Rettir | undefined>(
+  const [whatToShow, setWhatToShow] = React.useState<string | undefined>(
     undefined,
   );
 
-  const handleOnClick = (type: Rettir): void => {
+  const handleOnClick = (type: string): void => {
     setWhatToShow(type);
     setShow(true);
   };
 
+  const createButtons = (type): JSX.Element => (
+    <MFP.StyledButton label={`${type}`} onClick={() => handleOnClick(type)} />
+  );
+
+  const Menu = ({ type }: { type: string }): JSX.Element => (
+    <>
+      <h1> {type} </h1>
+      {(getDrinkOrFood()[type] as Food[]).map((v: Food) => (
+        <li>{v.price}</li>
+      ))}
+    </>
+  );
+
   return (
     <MFP.StyledBox fill>
-      <MFP.List>
-        <MFP.StyledButton
-          label="Smáréttir"
-          onClick={() => handleOnClick(Rettir.Smabraud)}
-        />
-        <MFP.StyledButton
-          label="Dönsk Rúgbrauð"
-          onClick={() => handleOnClick(Rettir.donsk)}
-        />
-        <MFP.StyledButton
-          label="Spænsk Súrdeigsbrauð"
-          onClick={() => handleOnClick(Rettir.spaensk)}
-        />
-        <MFP.StyledButton
-          label="Aðalréttir"
-          onClick={() => handleOnClick(Rettir.adal)}
-        />
-        <MFP.StyledButton
-          label="Barnamatseðill"
-          onClick={() => handleOnClick(Rettir.barna)}
-        />
-        <MFP.StyledButton
-          label="Salöt"
-          onClick={() => handleOnClick(Rettir.salot)}
-        />
-        <MFP.StyledButton
-          label="Mjúk Tortilla"
-          onClick={() => handleOnClick(Rettir.tortilla)}
-        />
-      </MFP.List>
+      {useRouter().pathname === `/drykkjarsida` ? (
+        <MFP.List>{Object.values(Drykkir).map(v => createButtons(v))}</MFP.List>
+      ) : (
+        <MFP.List>{Object.values(Rettir).map(v => createButtons(v))}</MFP.List>
+      )}
 
       {show && (
-        <MFP.StyledLayer
-          onEsc={() => setShow(false)}
-          animation="fadeIn"
-          onClickOutside={() => setShow(false)}
-        >
-          <ShowMenu WhatToShow={whatToShow} />
+        <MFP.StyledLayer>
+          <Menu type={whatToShow} />
           <MFP.StyledButton label="Tilbaka" onClick={() => setShow(false)} />
         </MFP.StyledLayer>
       )}
--- src/pages/drykkjarsida.tsx
+++ src/pages/drykkjarsida.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
-import { Carousel, MenuTab } from '@/components';
+import { useMediaQuery } from 'react-responsive';
+import { Carousel, Header, MenuForPhones, MenuTab } from '@/components';
 import * as FakeData from '@/db.json';
 import { Layout } from '@/Layouts';
 import { images } from '@/FakeData';
+import { Box } from 'grommet';
+import { GlobalStyle } from '@/Styles';
 
-const Drykkjarsedill: React.FC<{}> = () => (
-  <Layout
-    PhotoContainer={<Carousel seconds={8} images={images} />}
-    TextContainer={<MenuTab MapType="Drykkir" Map={FakeData.drykkir} />}
-  />
-);
+const Drykkjarsedill: React.FC<{}> = () => {
+  const [showPhone, setShowPhone] = React.useState<boolean>(false);
+
+  const phoneSize = useMediaQuery({
+    query: `(max-device-width: 600px)`,
+  });
+  React.useEffect(() => {
+    if (phoneSize) {
+      setShowPhone(true);
+    }
+  }, [showPhone]);
+  return (
+    <Box fill="vertical">
+      <GlobalStyle />
+      {showPhone ? (
+        <>
+          <Header />
+          <MenuForPhones />
+        </>
+      ) : (
+        <Layout
+          PhotoContainer={<Carousel seconds={8} images={images} />}
+          TextContainer={<MenuTab MapType="Drykkir" Map={FakeData.drykkir} />}
+        />
+      )}
+    </Box>
+  );
+};
 export default Drykkjarsedill;
