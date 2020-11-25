ea5193d0586258aca5d7800e9dd2625b256f00d8
Guðmundur Snorri Sigfússon <gummehh@gmail.com>
2020-11-25 17:46:48 +0000

    fix(public/phtoos): rename file name

--- src/FakeData.ts
+++ src/FakeData.ts
@@ -1,11 +1,7 @@
 /* eslint-disable object-curly-newline */
 
 const images = [`/Restaurant2.jpg`, `/haed-matur.jpg`];
-const HomeImages = [
-  `/tacos.jpg`,
-  `/restaurant.jpg
-`,
-];
+const HomeImages = [`/tacos.jpg`, `/restaurant.jpg`];
 
 const LoadingImage = `/logo-rotate.png`;
 const solidIcon = `/logotext.png`;
